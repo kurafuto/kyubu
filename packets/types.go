10ea346c991f317d5573ca9914903f258ece4153
Chris Carter <chris@gibsonsec.org>
2015-05-21 02:40:54 +1000

    Add some more types, + some tests for them.

--- packets/types.go
+++ packets/types.go
@@ -6,6 +6,7 @@ package packets
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 )
 
@@ -83,32 +84,110 @@ func (t *Short) Encode(buf *bytes.Buffer) error {
 	return binary.Write(buf, Endianness, t)
 }
 
-func (t *Short) Decode(buf *bytes.Buffer) (err error) {
-	err = binary.Read(buf, Endianness, t)
-	return
+func (t *Short) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
 }
 
 // (2) 0 to 65535
 // Unsigned 16-bit integer
 type UShort uint16
 
+func (t *UShort) Encode(buf *bytes.Buffer) error {
+	return binary.Write(buf, Endianness, t)
+}
+
+func (t *UShort) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
+}
+
 // (4) -2147483648 to 2147483647
 // Signed 32-bit integer, two's complement
 type Int int32
+
+func (t *Int) Encode(buf *bytes.Buffer) error {
+	return binary.Write(buf, Endianness, t)
+}
+
+func (t *Int) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
+}
 
 // (8) -9223372036854775808 to 9223372036854775807
 // Signed 64-bit integer, two's complement
 type Long int64
 
+func (t *Long) Encode(buf *bytes.Buffer) error {
+	return binary.Write(buf, Endianness, t)
+}
+
+func (t *Long) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
+}
+
 // (4) Single-precision 32-bit IEEE 754 floating point
 type Float float32
+
+func (t *Float) Encode(buf *bytes.Buffer) error {
+	return binary.Write(buf, Endianness, t)
+}
+
+func (t *Float) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
+}
 
 // (8) Double-precision 64-bit IEEE 754 floating point
 type Double float64
 
+func (t *Double) Encode(buf *bytes.Buffer) error {
+	return binary.Write(buf, Endianness, t)
+}
+
+func (t *Double) Decode(buf *bytes.Buffer) error {
+	return binary.Read(buf, Endianness, t)
+}
+
 // (>= 1, <= 2147483652) A sequence of Unicode code points
 // UTF-8 string prefixed with its size in bytes as a VarInt
 type String string
+
+func (t *String) Encode(buf *bytes.Buffer) (err error) {
+	// TODO: defer + recover() for PutVarint.
+	// TODO: This might not be efficient. Pool []byte arrays?
+	x := make([]byte, binary.MaxVarintLen32)
+	b := []byte(*t)
+
+	n := binary.PutVarint(x, int64(len(b)))
+
+	err = binary.Write(buf, Endianness, x[:n])
+	if err != nil {
+		return
+	}
+
+	err = binary.Write(buf, Endianness, b)
+	if err != nil {
+		return
+	}
+	return nil
+}
+
+func (t *String) Decode(buf *bytes.Buffer) error {
+	n, err := binary.ReadVarint(buf)
+	if err != nil {
+		return err
+	}
+
+	b := make([]byte, n)
+	n1, err := buf.Read(b)
+	if err != nil {
+		return err
+	} else if int64(n1) != n {
+		// TODO: delta := n - n1; Read()
+		return errors.New("didn't read enough bytes for string")
+	}
+
+	*t = String(b)
+	return nil
+}
 
 // (>= 1, <= 5) -2147483648 to 2147483647
 // Read with encoding/binary.ReadVarint
@@ -130,7 +209,11 @@ type NBTTag x
 
 // (8) x (-33554432 to 33554431), y (-2048 to 2047), z (-33554432 to 33554431)
 // x as a 26-bit integer, followed by y as a 12-bit integer, followed by z as a 26-bit integer
-type Position int64
+type Position struct {
+	X int32
+	Y int16
+	Z int32
+}
 
 // (1) Rotation angle in steps of 1/256 of a full turn
 type Angle byte
--- packets/types_test.go
+++ packets/types_test.go
@@ -205,3 +205,293 @@ func TestShortDecode(t *testing.T) {
 		t.Fatalf("expected 32767, got %d", b)
 	}
 }
+
+func TestUShortEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	var b = UShort(0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = UShort(65535)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0xff, 0xff}, buf.Bytes()) {
+		t.Fatalf("expected {0xff, 0xff}, got %#v", buf.Bytes())
+	}
+
+}
+
+func TestUShortDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x00, 0x00})
+
+	var b UShort
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 0 {
+		t.Fatalf("expected 0, got %d", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0xff, 0xff})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 65535 {
+		t.Fatalf("expected 32767, got %d", b)
+	}
+}
+
+func TestIntEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	var b = Int(0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Int(-2147483648)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x80, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x80, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Int(2147483647)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x7f, 0xff, 0xff, 0xff}, buf.Bytes()) {
+		t.Fatalf("expected {0x7f, 0xff, 0xff, 0xff}, got %#v", buf.Bytes())
+	}
+
+}
+
+func TestIntDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00})
+
+	var b Int
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 0 {
+		t.Fatalf("expected 0, got %d", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x80, 0x00, 0x00, 0x00})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != -2147483648 {
+		t.Fatalf("expected -2147483648, got %d", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x7f, 0xff, 0xff, 0xff})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 2147483647 {
+		t.Fatalf("expected 2147483647, got %d", b)
+	}
+}
+
+func TestLongEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	var b = Long(0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Long(-9223372036854775808)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Long(9223372036854775807)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, buf.Bytes()) {
+		t.Fatalf("expected {0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, got %#v", buf.Bytes())
+	}
+
+}
+
+func TestLongDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+	var b Long
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 0 {
+		t.Fatalf("expected 0, got %d", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != -9223372036854775808 {
+		t.Fatalf("expected -9223372036854775808, got %d", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 9223372036854775807 {
+		t.Fatalf("expected 9223372036854775807, got %d", b)
+	}
+}
+
+func TestFloatEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	var b = Float(0.0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Float(2139095039.0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x4e, 0xff, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x4e, 0xff, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+}
+
+func TestFloatDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00})
+
+	var b Float
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 0.0 {
+		t.Fatalf("expected 0.0, got %f", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x4e, 0xff, 0x00, 0x00})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 2139095039.0 {
+		t.Fatalf("expected 2139095039.0, got %f", b)
+	}
+}
+
+func TestDoubleEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+
+	var b = Double(0.0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+
+	buf.Reset()
+	b = Double(3423266188008119638567638907689404624415750050196511731360612015004450816.0)
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte{0x4e, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, buf.Bytes()) {
+		t.Fatalf("expected {0x4e, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, got %#v", buf.Bytes())
+	}
+}
+
+func TestDoubleDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+	var b Double
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 0.0 {
+		t.Fatalf("expected 0.0, got %f", b)
+	}
+
+	buf = bytes.NewBuffer([]byte{0x4e, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if b != 3423266188008119638567638907689404624415750050196511731360612015004450816.0 {
+		t.Fatalf("expected 3423266188008119638567638907689404624415750050196511731360612015004450816.0, got %f", b)
+	}
+}
+
+func TestStringEncode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+
+	var b = String("Hello, world!")
+	if err := b.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	exp := []byte{0x1a, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21}
+	if !bytes.Equal(exp, buf.Bytes()) {
+		t.Fatalf("expected %v, got %#v", exp, buf.Bytes())
+	}
+}
+
+func TestStringDecode(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{0x1a, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21})
+
+	var b String
+	if err := b.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	exp := String("Hello, world!")
+	if b != exp {
+		t.Fatalf("expected %q, got %q", exp, b)
+	}
+}
